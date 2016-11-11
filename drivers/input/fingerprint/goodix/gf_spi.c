241f7dab8821825ae80d71d55c8837c1a012ddd9
Louis Popi <theh2o64@gmail.com>
2016-11-11 16:07:35 +0100

    input: fingerprint: goodix: Switch from wakelocks to wakeup sources
    Change-Id: I15fa0a47dce4fbf26ddaa29ed77ecd884288fe2d

--- drivers/input/fingerprint/goodix/gf_spi.c
+++ drivers/input/fingerprint/goodix/gf_spi.c
@@ -40,7 +40,6 @@
 #include <linux/fb.h>
 #include <linux/pm_qos.h>
 #include <linux/cpufreq.h>
-#include <linux/wakelock.h>
 #ifdef OEM_DEBUG_SUPPORT
 #include <linux/oneplus/boot_mode.h>
 #endif
@@ -59,7 +58,7 @@
 #define VER_MINOR   2
 #define PATCH_LEVEL 1
 
-#define WAKELOCK_HOLD_TIME 500 /* in ms */
+#define WAKEUP_HOLD_TIME 500 /* in ms */
 
 #define GF_SPIDEV_NAME     "goodix,fingerprint"
 /*device name after register in charater*/
@@ -75,7 +74,7 @@ static int SPIDEV_MAJOR;
 static DECLARE_BITMAP(minors, N_SPI_MINORS);
 static LIST_HEAD(device_list);
 static DEFINE_MUTEX(device_list_lock);
-static struct wake_lock fp_wakelock;
+static struct wakeup_source fp_wakeup;
 static struct gf_dev gf;
 
 struct gf_key_map maps[] = {
@@ -495,7 +494,7 @@ static irqreturn_t gf_irq(int irq, void *handle)
 #if defined(GF_NETLINK_ENABLE)
 	char temp = GF_NET_EVENT_IRQ;
 
-	wake_lock_timeout(&fp_wakelock, msecs_to_jiffies(WAKELOCK_HOLD_TIME));
+	__pm_wakeup_event(&fp_wakeup, WAKEUP_HOLD_TIME);
 	sendnlmsg(&temp);
 #elif defined GF_FASYNC
 	struct gf_dev *gf_dev = &gf;
@@ -794,7 +793,7 @@ static int gf_probe(struct platform_device *pdev)
 
 	gf_dev->irq = gf_irq_num(gf_dev);
 
-	wake_lock_init(&fp_wakelock, WAKE_LOCK_SUSPEND, "fp_wakelock");
+	wakeup_source_init(&fp_wakeup, "fp_wakeup");
 	status = request_threaded_irq(gf_dev->irq, NULL, gf_irq,
 			IRQF_TRIGGER_RISING | IRQF_ONESHOT,
 			"gf_fp", gf_dev);
@@ -855,7 +854,7 @@ static int gf_remove(struct platform_device *pdev)
 {
 	struct gf_dev *gf_dev = &gf;
 
-	wake_lock_destroy(&fp_wakelock);
+	wakeup_source_trash(&fp_wakeup);
 	/* make sure ops on existing fds can abort cleanly */
 	if (gf_dev->irq)
 		free_irq(gf_dev->irq, gf_dev);
