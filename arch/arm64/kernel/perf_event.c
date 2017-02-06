f10ac988913e24af3c1c37b00b940d1be14dfa47
liwei <liwei@oneplus.net>
2017-02-06 21:28:25 +0800

    [16859]:[BSP][Stability][CBG-338] Add cpu_pmu->hw_events null judgment
    Plan:cpu_pmu->hw_events is not initialized when armv8pmu_idle_update is called.
    So we  need more condition to judge whether hw_events is initialized
    Type: BUG
    Test: Do reboot test
    *
    
    Change-Id: Iba5905a08ba1f8e0a3cc82b3871b5b2172a0baa6

--- arch/arm64/kernel/perf_event.c
+++ arch/arm64/kernel/perf_event.c
@@ -585,7 +585,11 @@ static void armv8pmu_idle_update(struct arm_pmu *cpu_pmu)
 	struct perf_event *event;
 	int idx;
 
+#ifdef VENDOR_EDIT
+	if (!cpu_pmu || !(cpu_pmu->hw_events))
+#else
 	if (!cpu_pmu)
+#endif
 		return;
 
 	hw_events = this_cpu_ptr(cpu_pmu->hw_events);
