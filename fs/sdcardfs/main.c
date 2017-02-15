4fff5e2453b5517ac0c836daf8c9fd6f6f65374d
liochen <lio.chen@oneplus.net>
2017-02-15 17:12:39 +0800

    [16859]:[filesystem] Fix OTG unmount take too long time issue
    Plan: Fix OTG unmount take too long time issue
    Type: BUG
    Test: Plug in/out OTG device
          Check OTG mount/unmount status
    Note: Revert ANDROID: sdcardfs: switch to full_name_hash and qstr
                 ANDROID: sdcardfs: Add GID Derivation to sdcardfs
    
    Change-Id: Ic4b0a4e7207d662a4ea4039ad6b667ff356e2da6

--- fs/sdcardfs/main.c
+++ fs/sdcardfs/main.c
@@ -329,13 +329,13 @@ static int sdcardfs_read_super(struct vfsmount *mnt, struct super_block *sb,
 	sb_info->obbpath_s = kzalloc(PATH_MAX, GFP_KERNEL);
 	mutex_lock(&sdcardfs_super_list_lock);
 	if(sb_info->options.multiuser) {
-		setup_derived_state(sb->s_root->d_inode, PERM_PRE_ROOT, sb_info->options.fs_user_id, AID_ROOT, false, sb->s_root->d_inode);
+		setup_derived_state(d_inode(sb->s_root), PERM_PRE_ROOT, sb_info->options.fs_user_id, AID_ROOT, false, d_inode(sb->s_root));
 		snprintf(sb_info->obbpath_s, PATH_MAX, "%s/obb", dev_name);
 		/*err =  prepare_dir(sb_info->obbpath_s,
 					sb_info->options.fs_low_uid,
 					sb_info->options.fs_low_gid, 00755);*/
 	} else {
-		setup_derived_state(sb->s_root->d_inode, PERM_ROOT, sb_info->options.fs_user_id, AID_ROOT, false, sb->s_root->d_inode);
+		setup_derived_state(d_inode(sb->s_root), PERM_ROOT, sb_info->options.fs_user_id, AID_ROOT, false, d_inode(sb->s_root));
 		snprintf(sb_info->obbpath_s, PATH_MAX, "%s/Android/obb", dev_name);
 	}
 	fixup_tmp_permissions(d_inode(sb->s_root));
