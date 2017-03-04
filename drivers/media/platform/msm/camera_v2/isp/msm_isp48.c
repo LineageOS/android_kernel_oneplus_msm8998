a4cf4cf41ef0b7d6f678124a1e691acc3ab9fc56
longxiaowu <shawn.loong@oneplus.net>
2017-03-04 15:41:55 +0800

    [16859]:[Camera][kernel] 修正基线升级版本代码差异
     Plan: None
     Type: USER
     Test: camera normal work
    
    Change-Id: If7dca5449eb914c660c7c96e405f30e4bba07803

--- drivers/media/platform/msm/camera_v2/isp/msm_isp48.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp48.c
@@ -218,6 +218,12 @@ static int msm_vfe48_get_clks(struct vfe_device *vfe_dev)
 		if (strcmp(vfe_dev->vfe_clk_info[i].clk_name,
 					"mnoc_maxi_clk") == 0)
 			vfe_dev->vfe_clk_info[i].clk_rate = INIT_RATE;
+		if (strcmp(vfe_dev->vfe_clk_info[i].clk_name,
+					"camss_vfe_clk") == 0)
+			vfe_dev->vfe_clk_info[i].clk_rate = INIT_RATE;
+		if (strcmp(vfe_dev->vfe_clk_info[i].clk_name,
+					"camss_vfe_stream_clk") == 0)
+			vfe_dev->vfe_clk_info[i].clk_rate = INIT_RATE;
 		/* set no memory retention */
 		if (strcmp(vfe_dev->vfe_clk_info[i].clk_name,
 				"camss_vfe_clk") == 0 ||
