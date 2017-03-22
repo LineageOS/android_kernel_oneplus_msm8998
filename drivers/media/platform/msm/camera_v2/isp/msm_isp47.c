fd3cacd4b11ecb73a60a9fef77a9d8b22bd4eb3c
niqiangbo <bob.nill@oneplus.net>
2017-03-22 14:33:17 +0800

    [16859]:[Camera] Fix incorrect merge in msm_isp47
    Plan:Fix incorrect merge in msm_isp47
    Type:USER
    Test:check camera normal function
    Note:1858589.patch
    
    From 30fafadebfd5c73277474429561ce9a51da67827 Mon Sep 17 00:00:00 2001
    From: Shubhraprakash Das <sadas@codeaurora.org>
    Date: Thu, 2 Mar 2017 15:43:06 -0800
    Subject: [PATCH] msm: camera: isp: Set the clock rate for camss vfe clock
    
    Make sure rate for camss vfe clock is set before enabling it.
    Also, reuse existing variables for the vfe src clk.
    
    Change-Id: Ic4acd4c8330b9300ea6bb84eb99a120453841f7a
    Signed-off-by: Shubhraprakash Das <sadas@codeaurora.org>
    
    Change-Id: I4542914dfca5f8a896574a011198c4f1b3eaa242

--- drivers/media/platform/msm/camera_v2/isp/msm_isp47.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp47.c
@@ -2564,20 +2564,16 @@ int msm_vfe47_set_clk_rate(struct vfe_device *vfe_dev, long *rate)
 {
 	int rc = 0;
 	int clk_idx = vfe_dev->hw_info->vfe_clk_idx;
-#ifndef VENDOR_EDIT
 	int ret;
-#endif
+
 	rc = msm_camera_clk_set_rate(&vfe_dev->pdev->dev,
 				vfe_dev->vfe_clk[clk_idx], *rate);
 	if (rc < 0)
 		return rc;
 	*rate = clk_round_rate(vfe_dev->vfe_clk[clk_idx], *rate);
-#ifdef VENDOR_EDIT //upgrade: need to check
 	vfe_dev->vfe_clk_info[clk_idx].clk_rate = *rate;
-#else
-	vfe_dev->msm_isp_vfe_clk_rate = *rate;
 	if (vfe_dev->vfe_cx_ipeak) {
-		if (vfe_dev->msm_isp_vfe_clk_rate >=
+		if (vfe_dev->vfe_clk_info[clk_idx].clk_rate >=
 			vfe_dev->vfe_clk_rates[MSM_VFE_CLK_RATE_TURBO]
 			[vfe_dev->hw_info->vfe_clk_idx] &&
 			vfe_dev->turbo_vote == 0) {
@@ -2596,7 +2592,7 @@ int msm_vfe47_set_clk_rate(struct vfe_device *vfe_dev, long *rate)
 				vfe_dev->turbo_vote = 0;
 		}
 	}
-#endif
+
 	if (vfe_dev->hw_info->vfe_ops.core_ops.ahb_clk_cfg)
 		vfe_dev->hw_info->vfe_ops.core_ops.ahb_clk_cfg(vfe_dev, NULL);
 	return 0;
