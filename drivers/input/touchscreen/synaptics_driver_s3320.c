02b4a97c43cd8a216508ba5d375dc15f1ae0a53d
XiNGRZ <chenxingyu92@gmail.com>
2017-07-04 22:04:46 +0800

    input: synaptics: s3320: hardcoded input_dev->name to "synaptics"
    otherwise it can't match the keylayout file name
    
    Change-Id: I4509a7583cff2636db05f3b0a97f757a0524045b

--- drivers/input/touchscreen/synaptics_driver_s3320.c
+++ drivers/input/touchscreen/synaptics_driver_s3320.c
@@ -2752,7 +2752,7 @@ static int	synaptics_input_init(struct synaptics_ts_data *ts)
 		return ret;
 	}
 
-	ts->input_dev->name = TPD_DEVICE;
+	ts->input_dev->name = "synaptics";
 	ts->input_dev->dev.parent = &ts->client->dev;
 	set_bit(EV_SYN, ts->input_dev->evbit);
 	set_bit(EV_ABS, ts->input_dev->evbit);
