e88784b2e49fe13b469955c27948931d3a0b0f72
Alexander Martinz <eviscerationls@gmail.com>
2017-08-15 15:08:28 +0200

    drivers: soc: qcom: import project_info
    This is a dependency for most OnePlus drivers.
    
    Change-Id: I490e053e27d79c436d739eda1d89d44f94d40926
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- drivers/soc/qcom/socinfo.c
+++ drivers/soc/qcom/socinfo.c
@@ -1559,6 +1559,10 @@ static void socinfo_select_format(void)
 	}
 }
 
+#ifdef CONFIG_ONEPLUS_PROJECT_INFO
+volatile uint32_t chip_serial_num;
+#endif
+
 int __init socinfo_init(void)
 {
 	static bool socinfo_init_done;
@@ -1586,6 +1590,12 @@ int __init socinfo_init(void)
 	boot_stats_init();
 	socinfo_print();
 	arch_read_hardware_id = msm_read_hardware_id;
+
+#ifdef CONFIG_ONEPLUS_PROJECT_INFO
+	/*read serial number*/
+	chip_serial_num = socinfo_get_serial_number();
+#endif
+
 	socinfo_init_done = true;
 
 	return 0;
