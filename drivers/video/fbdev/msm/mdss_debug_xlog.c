d2ff3d8c8083102303a17241c0bc4393cdce9c4f
Alexander Martinz <eviscerationls@gmail.com>
2017-08-16 21:58:21 +0200

    drivers: msm: import OnePlus mdss changes
    Change-Id: I5165282b3e11a081d7f6458c149214f7f1665cd5
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- drivers/video/fbdev/msm/mdss_debug_xlog.c
+++ drivers/video/fbdev/msm/mdss_debug_xlog.c
@@ -28,7 +28,11 @@
 #define XLOG_DEFAULT_ENABLE 0
 #endif
 
+#ifdef CONFIG_VENDOR_ONEPLUS
+#define XLOG_DEFAULT_PANIC 0
+#else
 #define XLOG_DEFAULT_PANIC 1
+#endif
 #define XLOG_DEFAULT_REGDUMP 0x2 /* dump in RAM */
 #define XLOG_DEFAULT_DBGBUSDUMP 0x2 /* dump in RAM */
 #define XLOG_DEFAULT_VBIF_DBGBUSDUMP 0x2 /* dump in RAM */
