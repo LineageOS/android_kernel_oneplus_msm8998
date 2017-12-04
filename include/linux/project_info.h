eda637a69356e1f04735821b853542f7e7e8a6b0
Alexander Martinz <alex@amartinz.at>
2017-12-04 17:31:20 +0100

    drivers: import from Open Beta 1
    Change-Id: I5a9edcfbbda3e2013815e9f6cc333bf98e05b090
    Signed-off-by: Alexander Martinz <alex@amartinz.at>

--- include/linux/project_info.h
+++ include/linux/project_info.h
@@ -1,43 +1,47 @@
 #ifndef _PROJECT_INFO_H_
 #define _PROJECT_INFO_H_ 1
 typedef		__u32		uint32;
-typedef unsigned char uint8;
+typedef     __u8        uint8;
 
 /*******SECURE_BOOTn = 0x00786078+ 0x4*n, n=[1..14]******/
 #define SECURE_BOOT_BASE		0x00786078
-#define SECURE_BOOT1			SECURE_BOOT_BASE + 0x4*1
+#define SECURE_BOOT1			(SECURE_BOOT_BASE + 0x4*1)
+#define BUF_SIZE		64
+
+extern uint32_t chip_serial_num;
+extern unsigned long totalram_pages __read_mostly;
 
 
-struct project_info{
-       char project_name[8];  //eg, 16859
-       uint32  hw_version;  //PCB number, T0, EVT
-       uint32  rf_v1;   //v1 for mainboard_rf_version
-       uint32  rf_v2;   //v2 for aboard_rf_version
-       uint32  rf_v3;
-       uint32  modem;
-       uint32  operator;
-       uint32  ddr_manufacture_info;
-       uint32  ddr_row;
-       uint32  ddr_column;
-       uint32  ddr_fw_version;
-       uint32  ddr_reserve_info;
-       uint32  platform_id;
-       uint32  ftm_uart_boot_mode;
-       uint32  feature_id;
+struct project_info {
+	char project_name[8];
+	uint32  hw_version;
+	uint32  rf_v1;
+	uint32  rf_v2;
+	uint32  rf_v3;
+	uint32  modem;
+	uint32  operator;
+	uint32  ddr_manufacture_info;
+	uint32  ddr_row;
+	uint32  ddr_column;
+	uint32  ddr_fw_version;
+	uint32  ddr_reserve_info;
+	uint32  platform_id;
+	uint32  ftm_uart_boot_mode;
+	uint32  feature_id;
 };
 
-struct component_info{
+struct component_info {
 	char *version;
 	char *manufacture;
 };
 
-enum{
+enum {
 	HW_VERSION__UNKNOWN,
-	HW_VERSION__11 = 11,//all EVB
-	HW_VERSION__12, 	//T0
+	HW_VERSION__11 = 11,
+	HW_VERSION__12,
 };
 
-enum COMPONENT_TYPE{
+enum COMPONENT_TYPE {
 	DDR,
 	EMMC,
 	F_CAMERA,
@@ -64,7 +68,8 @@ enum COMPONENT_TYPE{
 };
 
 
-int push_component_info(enum COMPONENT_TYPE type, char *version, char * manufacture);
+int push_component_info(enum COMPONENT_TYPE type,
+	char *version, char *manufacture);
 int reset_component_info(enum COMPONENT_TYPE type);
 uint32 get_hw_version(void);
 
