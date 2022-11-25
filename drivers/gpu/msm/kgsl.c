3f88a536d6a1be2974e97b66cde80caf93125155
Georg Veichtlbauer <georg@vware.at>
2022-11-25 08:47:16 +0100

    Remove OnePlus Virtual Reserve memory
    Change-Id: I1978ae360b80c59660f0796b56b3dc1dfc57cafe

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -4549,29 +4549,6 @@ static unsigned long _get_svm_area(struct kgsl_process_private *private,
 	 * Search downwards from the hint first. If that fails we
 	 * must try to search above it.
 	 */
-	if (current->mm->va_feature & 0x2) {
-		uint64_t lstart, lend;
-		unsigned long lresult;
-
-		switch (len) {
-		case 4096: case 8192: case 16384: case 32768:
-		case 65536: case 131072: case 262144:
-			lend = current->mm->va_feature_rnd
-			- (dbg_pm[2] * (ilog2(len) - dbg_pm[1]));
-			lstart =
-				current->mm->va_feature_rnd
-				- (dbg_pm[2] * dbg_pm[3]);
-			if (lend <= svm_end && lstart >= svm_start) {
-				lresult =
-				_search_range(private, entry
-						, lstart, lend, len, align);
-				if (!IS_ERR_VALUE(lresult))
-					return lresult;
-			}
-		default:
-			break;
-		}
-	}
 	result = _search_range(private, entry, start, addr, len, align);
 	if (IS_ERR_VALUE(result) && hint != 0)
 		result = _search_range(private, entry, addr, end, len, align);
